cc31cf824b8ab00bf6419b1583d4dd803fb1194e
Franco Marino <55522095+FrancoMarino@users.noreply.github.com>
2019-10-28 15:22:43 -0400

    T002 (#2)
    * T001: Agregar clase impresora
    
    * Añadir funcionalidades imprimir y obtener trabajos en cola
    
    * Metodos estaImprimiendo y obtenerTrabajoParaImprimir (Sin Pruebas)
    
    * Cambio en acceso a metodos

--- src/impresoraucab/Impresora.java
+++ src/impresoraucab/Impresora.java
@@ -5,31 +5,86 @@
  */
 package impresoraucab;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 /**
  *
  * @author josea
  */
 public class Impresora {
+
+    private List<Trabajo> colaDeImpresion = new ArrayList<>();
+    private boolean estado;
+
     //Constructor
-    public void Impresora(){
-        
-    }
-    
-    public void obtenerTrabajosEnCola(){
-    //obtiene la cantidad de trabajos (número entero positivo) encolados que deben ser impresos
-    
-    }
-    
-    public void pausarImpresion(){
-    //obtiene un estado (booleano) de encendido o apagado
-    }
-    
-    public void estaImprimiendo(){
-    //permite conocer si se esta imprimiendo un trabajo, que anteriormente haya estado encolado.
+    public void Impresora() {
+
     }
 
-    public void obtenerTrabajoParaImprimir(){
-    //obtiene el trabajo proximo a ser impreso.
-    
+    private int obtenerTrabajosEnCola() {
+        //obtiene la cantidad de trabajos (número entero positivo) encolados que deben ser impresos
+        int cont = 0;
+        for (Trabajo trabajo : colaDeImpresion) {
+            if (trabajo.getEstatus() == Estatus.IMPRIMIR) {
+                cont++;
+            }
+        }
+        return cont;
     }
+
+    private void pausarImpresion() {
+        //obtiene un estado (booleano) de encendido o apagado
+    }
+
+    private void estaImprimiendo(Trabajo trabajo) {
+        //permite conocer si se esta imprimiendo un trabajo, que anteriormente haya estado encolado.
+        if (this.colaDeImpresion.contains(trabajo)) {
+            if (trabajo.getEstatus() == Estatus.IMPRIMIENDO) {
+                this.estado = true;
+            } else {
+                this.estado = false;
+            }
+        } else {
+            this.estado = false;
+        }
+    }
+
+    private Trabajo obtenerTrabajoParaImprimir() {
+        //obtiene el trabajo proximo a ser impreso.
+        Trabajo trabajoEncontrado = null;
+        for (Trabajo trabajo : colaDeImpresion) {
+            if (trabajo.getEstatus() == Estatus.IMPRIMIR) {
+                trabajoEncontrado = trabajo;
+                break;
+            }
+        }
+        return trabajoEncontrado;
+    }
+
+    private boolean imprimir(Trabajo trabajo) throws InterruptedException {
+        TimeUnit.SECONDS.sleep(5); // ESPERA QUE SIMULA IMPRESION
+        int maxValor = 5;
+        int minValor = 1;
+        int estatusFinal = (int) Math.floor(Math.random() * (-maxValor + minValor) + minValor); //SIMULAR ESTATUS DE IMPRESION
+        if (estatusFinal == 1) {
+            return false; // NO SE IMPRIMIO
+        } else {
+            return true; // SE IMPRIMIO CORRECTAMENTE
+        }
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public List<Trabajo> getTrabajos() {
+        return colaDeImpresion;
+    }
+
 }
---
+++ src/impresoraucab/Trabajo.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package impresoraucab;
+
+/**
+ *
+ * @author josea
+ */
+public class Trabajo {
+    private Estatus estatus;
+    private String contenido;
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+    
+    public void Trabajo(Estatus estatus){
+        this.estatus = estatus;
+    }
+
+    public void setEstatus(Estatus estatus) {
+        this.estatus = estatus;
+    }
+
+    public Estatus getEstatus() {
+        return estatus;
+    }
+}
