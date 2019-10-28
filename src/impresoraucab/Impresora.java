c46836afae7f435a8f9183b1247ac3257243556c
José Andrés Fernández <joseandresfr1@gmail.com>
2019-10-27 22:29:17 -0400

    T001: Agregar clase impresora (#1)

---
+++ src/impresoraucab/Impresora.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package impresoraucab;
+
+/**
+ *
+ * @author josea
+ */
+public class Impresora {
+    //Constructor
+    public void Impresora(){
+        
+    }
+    
+    public void obtenerTrabajosEnCola(){
+    //obtiene la cantidad de trabajos (número entero positivo) encolados que deben ser impresos
+    
+    }
+    
+    public void pausarImpresion(){
+    //obtiene un estado (booleano) de encendido o apagado
+    }
+    
+    public void estaImprimiendo(){
+    //permite conocer si se esta imprimiendo un trabajo, que anteriormente haya estado encolado.
+    }
+
+    public void obtenerTrabajoParaImprimir(){
+    //obtiene el trabajo proximo a ser impreso.
+    
+    }
+}
